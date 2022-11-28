e5f33ed103adf8465b3bddaa17f99a4336726131
DashaKarpova <112941801+DashaKarpova@users.noreply.github.com>
2022-11-28 20:17:26 +0300

    кодирует заглавными буквами

--- Karpova/Shifr_Hilla/Shifr_Hilla.cpp
+++ Karpova/Shifr_Hilla/Shifr_Hilla.cpp
@@ -11,36 +11,59 @@ using namespace std::chrono;
 std:: vector<int> key = { 1, 2, 3, 7, 5, 4, 8, 2, 7 };
 
 //алфавит
+std::string alphabet() {
+	std::string alpabet = " ";
+	for (int i = 'A'; i <= 'Z'; i++)
+		alpabet += (char)i;
+	for (int i = 'a'; i <= 'z'; i++)
+		alpabet += (char)i;
+	return alpabet;
+}
 
+
+void output(int n) {
+
+	std::cout << n << std::endl;
+}
 
 int Hill_encryption(std::string word)
 {
-	//std::string word;
-	//std:: cout << "\nInput word";
-	//std:: cin.clear();
-	//std:: cin.ignore(INT_MAX, '\n');
-	//getline(std:: cin, word);
- 
 	auto start = steady_clock::now();
 
 	if ((word.length() != 0) and (word.length() % 3 != 0))
 	{
 		int amount = 3 - word.length() % 3;
 		for (int i(0); i < amount; ++i)
-			word += '#';
+			word += ' ';
 	}
-	//cout << endl << word.length() <<" "<< word;
+	std::string symbols;
+	symbols = alphabet();
+
+	for (int s = 0; s < word.length(); s++) {
+		for (int c = 0; c < symbols.length(); c++)
+		{
+			if (word[s] == symbols[c])
+			{
+				word[s] = c;
+				//std::cout << c << std::endl;
+			}
+		}
+	}
+	//std::cout << word << std::endl;
+
 
 	int i(0);
-	std:: string outputword = "";
+	std::string outputword = "";
 	while (i < word.length() - 1) {
 		for (int j(0); j < 3; ++j) {
-			//при шифровании по Хиллу мы используем mod52 для замены символов(так как в вводятся заглавные и строчные буквы английского алфавита)
-			int letter = (word[i] * key[j * 3 + 0] + word[i + 1] * key[j * 3 + 1] + word[i + 2] * key[j * 3 + 2]) % 52;
-			outputword += (char)letter;
+			//при шифровании по Хиллу мы используем mod26 для замены символов(так как в вводятся заглавные и строчные буквы английского алфавита)
+			int letter = (word[i] * key[j * 3 + 0] + word[i + 1] * key[j * 3 + 1] + word[i + 2] * key[j * 3 + 2]) % 26;
+			outputword += symbols[letter+1];
 		}
 		i += 3;
 	}
+
+
 
 	auto end = steady_clock::now();
 	auto elapsed_ms = duration_cast<microseconds>(end - start);
@@ -48,7 +71,7 @@ int Hill_encryption(std::string word)
 	//std::cout << std::endl << outputword << std::endl;
 	//std::cout << std::endl << elapsed_ms.count() << std::endl;
 	
-
+	output(elapsed_ms.count());
 	return elapsed_ms.count();
 }
 
