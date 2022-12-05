2dc000e24c02f50d9c1b0e31c53808ebb1a0b6d1
n_syromyatnikov <112897508+Syrnik1307@users.noreply.github.com>
2022-12-06 01:35:45 +0300

    ну теперь вообще точно круто

--- Сукин(виженер)/sykin (3).h
+++ Сукин(виженер)/sykin (3).h
@@ -10,3 +10,10 @@ void decode(std::string message);
 
 std::string vigenereCode(std::string message, std::string key);
 std::string vigenereDecode(std::string message, std::string key);
+
+struct Shifr
+{
+	int a=3;
+	string word="hutjoidfl;swpva0esronbhkcpx;/a4etbgrcdx][.tugbtvraec[]dtugdsl;:)";
+	string keyword="abcdefghr";
+};
--- Сукин(виженер)/sykin.cpp
+++ Сукин(виженер)/sykin.cpp
@@ -5,104 +5,98 @@
 #include <chrono>
 #include <map>
 
-void code(std::string message);
-void decode(std::string message);
-std::string vigenereCode(std::string message, std::string key);
-std::string vigenereDecode(std::string message, std::string key);
+void code(std::string word);
+void decode(std::string word);
+std::string vigenereCode(std::string word, std::string keyword);
+std::string vigenereDecode(std::string word, std::string keyword);
 bool alphabetOnly = true;
 
-using namespace std::chrono;
+//using namespace std::chrono;
 
-std::map<char, char> my_map = { //.,-/!?<>{}[]^*+=#%
-    { 'A', '.' },
-    { 'B', '2' },
-    { 'C', '3' }
-};
-
-void code(std::string message) {
+void code(std::string word) {
     unsigned int choice = 0;    
 
-    std::string key;
+    std::string keyword;
 
-    std::cout << "Choose a key > ";
-    getline(std::cin, key);
+    //std::cout << "Choose a keyword > ";
+    getline(std::cin, keyword);
 
-    if(key.size() == 0)     { std::cout << "Key's length is 0, quitting"; return; }
+    if(keyword.size() == 0)     { std::cout << "keyword's length is 0, quitting"; return; }
     auto start_1 = system_clock::now();
     
-    std::string result = vigenereCode(message, key);
+    std::string result = vigenereCode(word, keyword);
 
-    std::cout << "Result : " << result;
-    auto finish_1 = system_clock::now();
-    auto duration_1 = duration_cast<microseconds>(finish_1-start_1).count();
-    std::cout << "\nEncrypt time: "<< duration_1 << " microseconds"<< std::endl;
+    //std::cout << "Result : " << result;
+    //auto finish_1 = system_clock::now();
+    //auto duration_1 = duration_cast<microseconds>(finish_1-start_1).count();
+    //std::cout << "\nEncrypt time: "<< duration_1 << " microseconds"<< std::endl;
 }
 
-void decode(std::string message) {
+void decode(std::string word) {
     
-    std::string key;
+    std::string keyword;
 
-    std::cout << "Your key > ";
-    getline(std::cin, key);
+    //std::cout << "Your keyword > ";
+    getline(std::cin, keyword);
 
-    if(key.size() == 0)     { std::cout << "Key's length is 0, quitting"; return; }
+    if(keyword.size() == 0)     { std::cout << "keyword's length is 0, quitting"; return; }
 
     auto start_2 = system_clock::now();
-    std::string result = vigenereDecode(message, key);
+    std::string result = vigenereDecode(word, keyword);
 
-    std::cout << "Result : " << result;
-    auto finish_2 = system_clock::now();
-    auto duration_2 = duration_cast<microseconds>(finish_2-start_2).count();
-    std::cout << "\nDecrypt time: "<< duration_2 << " microseconds"<< std::endl;
+    //std::cout << "Result : " << result;
+    //auto finish_2 = system_clock::now();
+    //auto duration_2 = duration_cast<microseconds>(finish_2-start_2).count();
+    //std::cout << "\nDecrypt time: "<< duration_2 << " microseconds"<< std::endl;
 }
 
-std::string vigenereCode(std::string message, std::string key) {
-    unsigned int counterKey = 0, sizeOfKey = key.size();
+std::string vigenereCode(std::string word, std::string keyword) {
+    unsigned int counterkeyword = 0, sizeOfkeyword = keyword.size();
     char codedChar;
     std::string codedString;
     unsigned int placeOfChar;
     if(alphabetOnly) {
         std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,-/!?<>{}[]^*+=#%";
-        for(unsigned int i = 0; i < message.size(); i++) {                         //.,-/!?<>{}[]^*+=#%
-            if((placeOfChar = alphabet.find(message[i])) == std::string::npos)  { return "Error, characters are not part of the following string : " + alphabet; }
-            codedChar = (placeOfChar + key[counterKey]%53)%53;
+        for(unsigned int i = 0; i < word.size(); i++) {                         //.,-/!?<>{}[]^*+=#%
+            if((placeOfChar = alphabet.find(word[i])) == std::string::npos)  { return "Error, characters are not part of the following string : " + alphabet; }
+            codedChar = (placeOfChar + keyword[counterkeyword]%53)%53;
             //std::cout << codedChar << std::endl;
             codedString += alphabet[codedChar];
-            ++counterKey%=sizeOfKey;
+            ++counterkeyword%=sizeOfkeyword;
         }
     } else {
-        for(unsigned int i = 0; i < message.size(); i++) {
-            codedChar = (message[i] + key[counterKey])%256;
+        for(unsigned int i = 0; i < word.size(); i++) {
+            codedChar = (word[i] + keyword[counterkeyword])%256;
             
             codedString += codedChar;
-            ++counterKey%=sizeOfKey;
+            ++counterkeyword%=sizeOfkeyword;
         }
     }
     return codedString;
 }
 
-std::string vigenereDecode(std::string message, std::string key) {
-    unsigned int counterKey = 0, sizeOfKey = key.size();
+std::string vigenereDecode(std::string word, std::string keyword) {
+    unsigned int counterkeyword = 0, sizeOfkeyword = keyword.size();
     char decodedChar;
     std::string decodedString;
     unsigned int placeOfChar;
     if(alphabetOnly) {
         std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,-/!?’»<>{}[]^*+=#%’";
-        for(unsigned int i = 0; i < message.size(); i++) {
-            if((placeOfChar = alphabet.find(message[i])) == std::string::npos)  { return "Error, characters are not part of the following string : " + alphabet; }
-            decodedChar = (placeOfChar - (key[counterKey]%53));
+        for(unsigned int i = 0; i < word.size(); i++) {
+            if((placeOfChar = alphabet.find(word[i])) == std::string::npos)  { return "Error, characters are not part of the following string : " + alphabet; }
+            decodedChar = (placeOfChar - (keyword[counterkeyword]%53));
            // std::cout<< "1: " << (int)decodedChar << std::endl;
             if(decodedChar < 0)   { decodedChar+=53; }
             //std::cout<< "2: " << (int)decodedChar << std::endl;
             decodedString += alphabet[decodedChar];
-            ++counterKey%=sizeOfKey;
+            ++counterkeyword%=sizeOfkeyword;
         }
     } else {
-        for(unsigned int i = 0; i < message.size(); i++) {
-            decodedChar = (message[i] - key[counterKey]);
+        for(unsigned int i = 0; i < word.size(); i++) {
+            decodedChar = (word[i] - keyword[counterkeyword]);
             if(decodedChar < 0)   { decodedChar+=256; }
             decodedString += decodedChar;
-            ++counterKey%=sizeOfKey;
+            ++counterkeyword%=sizeOfkeyword;
         }
     }
     return decodedString;
