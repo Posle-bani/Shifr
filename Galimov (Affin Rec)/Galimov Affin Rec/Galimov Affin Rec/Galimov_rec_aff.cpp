129fd5a1d40d02dc72bad7ff6f6f74dba2c4012f
galim0v <flan2010@mail.ru>
2022-12-06 01:49:49 +0300

    delete slovo

--- Galimov (Affin Rec)/Galimov Affin Rec/Galimov Affin Rec/Galimov_rec_aff.cpp
+++ Galimov (Affin Rec)/Galimov Affin Rec/Galimov Affin Rec/Galimov_rec_aff.cpp
@@ -25,7 +25,7 @@ void zapoln_keys(Encryption key, vector <int>& alpha, vector <int>& beta)  //пер
     beta[1] = key.affrecb2;
 }
 
-void Galimov(string& slovo, Encryption key)  //сама функци€
+void Galimov(Encryption key)  //сама функци€
 {
     int kolvo = 0, poz = 0, sh_poz = 0;
     vector <char> alphavit(256);
@@ -33,10 +33,10 @@ void Galimov(string& slovo, Encryption key)  //сама функци€
     char el;
     zapoln_keys(key, alpha, beta);//ключи забираютс€ из структуры
     zapoln_alphavit(alphavit);//алфавит заполн€ю
-    kolvo = slovo.length();//смотрю количество букв в слове
+    kolvo = key.word.length();//смотрю количество букв в слове
     for (int i = 0; i < kolvo; i++)//на каждую букву шаг
     {
-        el = slovo[i];//беру нужную букву
+        el = key.word[i];//беру нужную букву
         vector<char>::iterator it = find(alphavit.begin(), alphavit.end(), el); //нахожу эту букву в алфавите и беру итератор
         if (it == alphavit.end())//проверка на нахождение буквы
         {
